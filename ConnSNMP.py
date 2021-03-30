56bfb66bbb80c753628082c1cd7a28ba93fbe649
Victor Bernardes <victor_luizbernardes5@outlook.com>
2021-03-30 04:13:05 -0300

    core functions working

--- ConnSNMP.py
+++
@@ -1,9 +0,0 @@
-from easysnmp import session
-
-class ConnSNMP:
-    def __init__(self, pw_priv = "", pw_auth = ""):
-        self.pw_priv = pw_priv
-        self.pw_auth = pw_auth
-        
-    def create_session(self):
-        session.Session()
